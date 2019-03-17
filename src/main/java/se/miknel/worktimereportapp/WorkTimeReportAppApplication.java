c6350fc3567369a799ca07107ccae5d0b2c7e879
teksah <teksah@gmail.com>
2019-03-17 15:57:00 +0100

    fix commit

---
+++ src/main/java/se/miknel/worktimereportapp/WorkTimeReportAppApplication.java
@@ -0,0 +1,13 @@
+package se.miknel.worktimereportapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkTimeReportAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkTimeReportAppApplication.class, args);
+    }
+
+}
