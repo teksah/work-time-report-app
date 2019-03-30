15248699fee12f3902c5ccc4ec6823362be7dc20
teksah <teksah@gmail.com>
2019-03-30 15:08:45 +0100

    add customer list page

--- report-app-web/src/main/java/se/miknel/worktimereportapp/controllers/CustomerController.java
+++ report-app-web/src/main/java/se/miknel/worktimereportapp/controllers/CustomerController.java
@@ -4,13 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import se.miknel.worktimereportapp.model.Customer;
+import se.miknel.worktimereportapp.services.CustomerService;
 
 @Controller
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 
     @GetMapping("/customers/new")
     public String initCreationForm(Model model) {
         model.addAttribute("customer", new Customer());
         return "customers/add-customer";
+    }
+
+    @GetMapping("/customers")
+    public String showAllCustomers(Model model) {
+        model.addAttribute("customers", customerService.findAll());
+
+        return "customers/list-customers";
     }
 }
