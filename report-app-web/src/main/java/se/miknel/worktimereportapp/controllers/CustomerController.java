3607b26adc428b4dcaa9ed7d466b71806d61b2ed
teksah <teksah@gmail.com>
2019-04-01 00:17:25 +0200

    add show customer page

--- report-app-web/src/main/java/se/miknel/worktimereportapp/controllers/CustomerController.java
+++ report-app-web/src/main/java/se/miknel/worktimereportapp/controllers/CustomerController.java
@@ -3,16 +3,20 @@ package se.miknel.worktimereportapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import se.miknel.worktimereportapp.model.Customer;
 import se.miknel.worktimereportapp.services.CustomerService;
+import se.miknel.worktimereportapp.services.ProjectService;
 
 @Controller
 public class CustomerController {
 
     private final CustomerService customerService;
+    private final ProjectService projectService;
 
-    public CustomerController(CustomerService customerService) {
+    public CustomerController(CustomerService customerService, ProjectService projectService) {
         this.customerService = customerService;
+        this.projectService = projectService;
     }
 
     @GetMapping("/customers/new")
@@ -26,5 +30,13 @@ public class CustomerController {
         model.addAttribute("customers", customerService.findAll());
 
         return "customers/list-customers";
+    }
+
+    @GetMapping("/customers/{customerId}")
+    public String showCustomer(@PathVariable("customerId") Long customerId, Model model) {
+        model.addAttribute("customer", customerService.findById(customerId));
+        model.addAttribute("pageTitle", customerService.findById(customerId));
+
+        return "/customers/show-customer";
     }
 }
