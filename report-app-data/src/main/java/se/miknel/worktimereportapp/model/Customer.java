9f1ae838679dd876edfc38d1c980e15a11c76f59
teksah <teksah@gmail.com>
2019-04-11 23:30:41 +0200

    add add function of customer entity

--- report-app-data/src/main/java/se/miknel/worktimereportapp/model/Customer.java
+++ report-app-data/src/main/java/se/miknel/worktimereportapp/model/Customer.java
@@ -2,6 +2,7 @@ package se.miknel.worktimereportapp.model;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.List;
 
--- report-app-web/src/main/java/se/miknel/worktimereportapp/controllers/CustomerController.java
+++ report-app-web/src/main/java/se/miknel/worktimereportapp/controllers/CustomerController.java
@@ -2,11 +2,15 @@ package se.miknel.worktimereportapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import se.miknel.worktimereportapp.model.Customer;
 import se.miknel.worktimereportapp.services.CustomerService;
 import se.miknel.worktimereportapp.services.ProjectService;
+
+import javax.validation.Valid;
 
 @Controller
 public class CustomerController {
@@ -34,8 +38,18 @@ public class CustomerController {
     }
 
     @GetMapping("/customers/new")
-    public String initCreationForm(Model model) {
-        model.addAttribute("customer", new Customer());
+    public String initCreationForm(Customer customer) {
         return "customers/add-customer";
+    }
+
+    @PostMapping("/customers/add")
+    public String addCustomer(@Valid Customer customer, BindingResult result) {
+        if (result.hasErrors()) {
+            return "customers/add-customer";
+        }
+
+        customerService.save(customer);
+
+        return "redirect:/customers/"+customer.getId()+"/show";
     }
 }
