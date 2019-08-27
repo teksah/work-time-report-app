4db1929fb335803a52d28b76d596da3154125a47
teksah <teksah@gmail.com>
2019-08-27 20:00:51 +0200

    add spring security add form based authentication add default user with a role and database load

---
+++ report-app-web/target/classes/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO unit_of_rest (value) VALUES (0);
+INSERT INTO unit_of_rest (value) VALUES (0.5);
+INSERT INTO unit_of_rest (value) VALUES (1);
+INSERT INTO unit_of_rest (value) VALUES (1.5);
+
+INSERT INTO worker_type (type) VALUES ('student');
+INSERT INTO worker_type (type) VALUES ('helper');
+INSERT INTO worker_type (type) VALUES ('electrician');
+
+
+
